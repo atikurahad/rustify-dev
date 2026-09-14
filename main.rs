0fbe6001fe796f7ab7c1865a7e2184f63c3eb2ac
atikurahad <atikurahad@gmail.com>
2026-09-14 16:55:53 +0600

    use pattern and print

--- main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let price = 500;
-    println!("Price is: {}", price);
-}
--- my-app/src/main.rs
+++ my-app/src/main.rs
@@ -6,3 +6,14 @@ fn main() {
     println!("Cart total: {}", cart_total);
     println!("Tax rate: {}", TAX_RATE);
 }
+
+
+fn mini() {
+    const student_id: u32 = 10; 
+    let  gpa : f64 = 3.85;
+    let is_passed : bool = true;
+    let section : char=  'B';
+    println!("{}", student_id,gpa,is_passed,section);
+   
+}
+
