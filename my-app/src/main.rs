bfa29a14dc920c02ba4c7ec93e76a72b61dc7a38
atikurahad <atikurahad@gmail.com>
2026-09-14 19:22:33 +0600

    func call and conditions

--- my-app/src/main.rs
+++ my-app/src/main.rs
@@ -1,19 +1,35 @@
+fn is_even(number: i32) -> bool {
+    if number % 2 == 0 {
+        true
+    } else {
+        false
+    }
+}
+
 fn main() {
-    const TAX_RATE: f64 = 0.05; 
-    let mut cart_total = 1000;
-
-    cart_total = cart_total + 200; 
-    println!("Cart total: {}", cart_total);
-    println!("Tax rate: {}", TAX_RATE);
+    let result1 = is_even(10);
+    let result2 = is_even(7);
+    println!("10 is even: {}", result1);
+    println!("7 is even: {}", result2);
 }
 
 
-fn mini() {
-    const student_id: u32 = 10; 
-    let  gpa : f64 = 3.85;
-    let is_passed : bool = true;
-    let section : char=  'B';
-    println!("{}", student_id,gpa,is_passed,section);
+// fn mini() {
+//     const student_id: u32 = 10; 
+//     let  gpa : f64 = 3.85;
+//     let is_passed : bool = true;
+//     let section : char=  'B';
+//     println!("{} {} {} {}", student_id, gpa, is_passed, section);
    
-}
+// }
+
+// fn discount() {
+//     let discount = "10"; 
+//     let  discount : u32 = discount.parse().expect("Not a valid number ");
+//     let discount = discount * 2;
+    
+//     println!("{}", discount);
+   
+// }
+
 
